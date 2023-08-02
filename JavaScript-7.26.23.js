ba861160a422af4932d022624de88cd00d0ce04d
jcastanonc <105474625+jcastanonc@users.noreply.github.com>
2023-08-02 15:12:48 -0700

    Add files via upload

---
+++ JavaScript-7.26.23.js
@@ -0,0 +1,9 @@
+let stopwatch = {}
+stopwatch.currentTime = 12
+stopwatch.tellTime = function(time) {
+    console.log(`The current time is ${time}`)
+}
+stopwatch.tellTime(stopwatch.currentTime)
+
+
+/
