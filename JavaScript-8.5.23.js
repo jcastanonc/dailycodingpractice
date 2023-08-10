ba7b3fee8e05448572b44741d1f696937c768da9
jcastanonc <105474625+jcastanonc@users.noreply.github.com>
2023-08-09 18:50:54 -0700

    Add files via upload

---
+++ JavaScript-8.5.23.js
@@ -0,0 +1,19 @@
+//Create a button that adds 1 to a botScore stored in localStorage 
+localStorage.setItem('botScore', 0)
+
+if(!localStorage.getItem('botScore')) {
+    localStorage.setItem('botScore',0)
+
+}
+
+document.querySelector('button').addEventListener('click', addAnothaOne)
+
+function addAnothaOne(){
+    let botScoreVal = Number(localStorage.getItem('botScore'))
+    botScoreVal += 1
+    localStorage.setItem('botScore', botScoreVal)
+  
+  
+
+ 
+}
