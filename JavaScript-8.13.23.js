15525fec18f3c5db18baea237a6d7864bb4ff0a7
jcastanonc <105474625+jcastanonc@users.noreply.github.com>
2023-08-14 21:54:48 -0700

    Add files via upload

---
+++ JavaScript-8.13.23.js
@@ -0,0 +1,11 @@
+//Loops
+/*You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. 
+Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are 
+battling has in their party*/
+function undergroundPokemon(partySize) {
+    for(let i=1; i <= partySize; i++){
+        console.log("I choose you!")
+}
+
+}
+undergroundPokemon(6)
