a69758fa99c5a3ef3ec154a3fe9bd6be91c60b69
jcastanonc <105474625+jcastanonc@users.noreply.github.com>
2023-08-15 20:31:40 -0700

    Add files via upload

---
+++ JavaScript-8.14.23.js
@@ -0,0 +1,12 @@
+//Arrays
+/*Your pokemon party order which is a list of pokemon has been leaked to Misty. 
+Please create a function that reverses your list and prints it to the console. */
+function reverseList(partyList) {    
+    console.log(partyList.reverse())
+}
+SreverseList(["bulba", "charmander", "pika", "butter"])
+
+
+
+
+
