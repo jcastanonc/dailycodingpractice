6b3dcdcafe1b34c063e65c9d13750df927d9b920
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2023-10-17 18:46:47 -0700

    october 7 and 8 2023

---
+++ JavaScript-10.8.23.js
@@ -0,0 +1,14 @@
+
+/*In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell). */
+function DNAStrand(dna) {
+  return dna.replace(/./g, function(c) {
+    return DNAStrand.pairs[c]
+  })
+}
+
+DNAStrand.pairs = {
+  A: 'T',
+  T: 'A',
+  C: 'G',
+  G: 'C',
+}
