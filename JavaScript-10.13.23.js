c491d7c87bb5c60bf74d364600c340380320b7d2
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2023-10-24 09:41:07 -0700

    practice thru 10.12.23 thru 10.13.23

---
+++ JavaScript-10.13.23.js
@@ -0,0 +1,10 @@
+/* Numbers ending with zeros are boring.
+They might be fun in your world, but not here.
+Get rid of them. Only the ending ones.
+*/
+function noBoringZeros(n) {
+  while (n % 10 === 0 && n !== 0) {
+    n = n / 10
+  }
+  return n
+}
