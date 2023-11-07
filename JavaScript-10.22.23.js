e580e1caf529c6a411a54662ad6c9755c7748db0
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2023-11-07 10:16:51 -0800

    practice from 10.22.23-10.24.23

---
+++ JavaScript-10.22.23.js
@@ -0,0 +1,7 @@
+/*Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) */
+function rowSumOddNumbers(n) {
+  //parameter is an odd number
+  //return sum of nth row
+  //example 2 --> 3 + 5 = 8
+  return Math.pow(n, 3);
+}
