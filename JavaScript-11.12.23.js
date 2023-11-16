5276f9a685187039005086d0585c51eb0577af9d
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2023-11-16 10:26:52 -0800

    practice 11.09.23 thru 11.12.23

---
+++ JavaScript-11.12.23.js
@@ -0,0 +1,6 @@
+/*Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary. */
+function sayHello( name, city, state ) {
+  //parameters: name, city, state
+  //return string Hello, John Smith! Welcome to Phoenix, Arizona!
+  return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
