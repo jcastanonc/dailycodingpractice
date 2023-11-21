d01136849ae68630556834a94829e6fd8f865614
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2023-11-21 10:41:55 -0800

    practice 11.21.23

---
+++ JavaScript-11.21.23.js
@@ -0,0 +1,8 @@
+/*Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be great.er than 2*/
+function angle(n) {
+  //parameter is n for number of sides in polygon
+  //return sum
+  return 180*(n-2);
+}
+
+
