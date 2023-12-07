f2d5b081fd14ec93cdd6cf21a5743f3fd8737b93
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2023-12-06 17:33:25 -0800

    practice 12.6.23

---
+++ JavaScript-12.6.23.js
@@ -0,0 +1,6 @@
+/* Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.*/
+function include(arr, item){
+  // parameters are an array of a list and the item
+  //return true if item belongs in array
+ return arr.includes(item);
+}
