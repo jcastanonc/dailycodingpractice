9a674304a5e3b61209d6acde73d972ce0bb79d8a
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2023-12-20 09:14:37 -0800

    practice 12.20.23

---
+++ JavaScript-12.20.23.js
@@ -0,0 +1,10 @@
+//simple node just http and fs
+const http = require('http')
+const fs = require('fs')
+http.createServer((req, res) => {
+    fs.readFile('demofile.html', (err, data) => {
+        res.writeHead(200, {'Content-Type' : 'text/html'})
+        res.write(data)
+        res.end()
+    })
+}).listen(8000)
