9e9695596729429fefd730667b7bda37657d4a08
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2024-01-03 09:55:03 -0800

    practice 12.26.23 and 12.27.23

---
+++ JavaScript-12.26.23.js
@@ -0,0 +1,16 @@
+/*Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+The Haskell version takes a list of directions with data Direction = North | East | West | South.
+The Clojure version returns nil when the path is reduced to nothing.
+The Rust version takes a slice of enum Direction {North, East, West, South}. */
+function dirReduc(plan) {
+  var opposite = {
+    'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
+  return plan.reduce(function(dirs, dir){
+      if (dirs[dirs.length - 1] === opposite[dir])
+        dirs.pop();
+      else
+        dirs.push(dir);
+      return dirs;
+    }, []);
+}
