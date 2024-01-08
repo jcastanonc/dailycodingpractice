de4c39083de739029565670f1dc18bb408124d1e
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2024-01-07 21:34:57 -0800

    practice 1.1.24 to 1.3.24

---
+++ JavaScript-1.1.24.js
@@ -0,0 +1,8 @@
+/*Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation. */
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    //parameters length, width, height
+    //return function for volume of a cuboid
+    return length * width * height
+  }
+}
