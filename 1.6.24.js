ce297d3c9a3b781d9ec6985bb043f7cc0a974e0b
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2024-01-10 09:09:26 -0800

    practice 1.6.24 to 1.7.24

---
+++ 1.6.24.js
@@ -0,0 +1,6 @@
+//example of Queue data structure
+let queue = [ ]
+queue.push(2) // queue is now [2]
+queue.push(5) //queue is now [2,5]
+let i = queue.shift() //queue is now [5]
+alert(i) // displays 2
---
+++ 1.7.24.js
@@ -0,0 +1,6 @@
+//example of stack data structure
+let stack = [ ]
+stack.push(2) // stack is now [2]
+stack.push(5) // stack is now [2, 5]
+let = stack.pop() // stack is now [2]
+alert(i) // displays 5
