d7a162eaa7b79102b04da19552a8854c4041a655
Jasmin Castanon-Carrillo <jasmincastanon@gmail.com>
2024-01-12 11:56:35 -0800

    practice 1.10.24 to 1.12.24

---
+++ JavaScript-1.10.24.js
@@ -0,0 +1,6 @@
+/*Write a function that removes the spaces from the string, then return the resultant string. */
+function noSpace(x){
+  //parameter is x a string
+  //return string without spaces
+  return x.split(" ").join("")
+}
